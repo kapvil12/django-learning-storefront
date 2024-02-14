2c2db6d0c94a3b17edef29dc88e9e39ade5e5961
Kopil Bhusal <kopil@ramailo.tech>
2024-02-15 00:02:52 +0545

    feat: initial commit

---
+++ playground/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+# request -> response / request handler (action but in django called as view)
+
+from django.http import HttpResponse
+
+def say_hello(request):
+    # pull data from db, transform data, send email ..
+    # return HttpResponse('Hello World')
+    return render(request, 'hello.html', {'name': 'Kopil'})
